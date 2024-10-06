530263ba41b60c8e616ce371b21d474b3b1520f3
fayssal dd <fayssaldd982@gmail.com>
2024-10-06 13:07:06 +0100

    ajouter nouvel disign for laptop

--- src/pages/Kuran.jsx
+++ src/pages/Kuran.jsx
@@ -49,81 +49,96 @@ export default function Kuran() {
         const uniqueRewayas = [...new Set(reciters.map(reciter => reciter.rewaya))];
         const recitersWithReways = rewayaSelect && reciters.filter(rc=>rc.rewaya == rewayaSelect )
   return (
-    <div dir='rtl' className='p-10 mt-12'>
-            <div className='flex max-sm:flex-col'>
-                <div className='mb-4 m-1 w-1/2 max-sm:w-full'>
-                    <label className='mb-4 block'>حدد الرواية</label>
-                    <Select onValueChange={(value)=>{
-                        setRewayaSelect(value)}}>
-                        <SelectTrigger className="w-full">
-                            <SelectValue placeholder="حدد الرواية" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            {
-                                uniqueRewayas?.map((rw,key)=>(
-                                    <SelectItem key={key} value={rw}>{rw}</SelectItem>
-                                ))
-                            }
-                        </SelectContent>
-                    </Select>
+    <div dir='rtl' className='p-10 lg:flex mt-12'>
+            <div className='lg:w-1/5 max-sm:w-full lg:ml-8 lg:shadow-xl lg:border lg:p-4'>
+                <div className='flex flex-col max-sm:flex-col'>
+                    <div className='mb-4 m-1  max-sm:w-full'>
+                        <label className='mb-4 block'>حدد الرواية</label>
+                        <Select onValueChange={(value)=>{
+                            setRewayaSelect(value)}}>
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="حدد الرواية" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {
+                                    uniqueRewayas?.map((rw,key)=>(
+                                        <SelectItem key={key} value={rw}>{rw}</SelectItem>
+                                    ))
+                                }
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className='mb-4 m-1 max-sm:w-full'>
+                        <label className='mb-4 block'>حدد القارئ </label>
+                        <Select onValueChange={(value)=>{
+                            setSurasReciter(value.suras)
+                            setReciterDetail(value)}}>
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder="حدد القارء" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {
+                                    recitersWithReways?.map((rc,key)=>(
+                                        <SelectItem key={key} value={rc}>{rc.name}</SelectItem>
+                                    ))
+                                }
+                            </SelectContent>
+                        </Select>
+                    </div>
                 </div>
-                <div className='mb-4 m-1 w-1/2 max-sm:w-full'>
-                    <label className='mb-4 block'>حدد القارئ </label>
-                    <Select onValueChange={(value)=>{
-                        setSurasReciter(value.suras)
-                        setReciterDetail(value)}}>
-                        <SelectTrigger className="w-full">
-                            <SelectValue placeholder="حدد القارء" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            {
-                                recitersWithReways?.map((rc,key)=>(
-                                    <SelectItem key={key} value={rc}>{rc.name}</SelectItem>
-                                ))
-                            }
-                        </SelectContent>
-                    </Select>
+                <div className='mb-8'>
+                    <p className='text-center mb-4'>
+                        قم بتحديد الرواية و القارئ أولا
+                    </p>
+                    <p className='flex max-sm:flex-col flex-col items-start justify-start'>
+                        <span className='max-sm:mx-0 mb-4'>
+                            <b className='underline'>القارء الحالي : </b>
+                            {reciterDetail?.name}
+                        </span>
+                        <span className='max-sm:mb-4'>
+                            <b  className='underline' >السورة الحالية : </b> 
+                            {surahPlayer?.name_arabic}
+                        </span>
+                        
+                    </p>
+                    <p className='w-full mt-4' >
+                            <Input  onChange={(e)=>{
+                                        console.log(e.target.value);
+                                        
+                                        setSurahSeachre(filteredSuras?.filter(sr=>sr.name_arabic.includes(e.target.value)))
+                                    }} 
+                                    className={'w-full'}
+                                type="text" placeholder="بحث عن سورة"
+                            />
+                    </p>
+            
                 </div>
-            </div>
-            <div>
-                <p className='text-center mb-4'>
-                    قم بتحديد الرواية و القارئ أولا
-                </p>
-                <p className='flex max-sm:flex-col items-center justify-start'>
-                    <span className='mx-8 max-sm:mx-0 max-sm:mb-4'>
-                        <b className='underline'>القارء الحالي : </b>
-                        {reciterDetail?.name}
-                    </span>
-                    <span className='max-sm:mb-4'>
-                        <b  className='underline' >السورة الحالية : </b> 
-                        {surahPlayer?.name_arabic}
-                    </span>
-                    
-                </p>
-                <p className='w-full mt-4' >
-                        <Input  onChange={(e)=>{
-                                    console.log(e.target.value);
-                                    
-                                    setSurahSeachre(filteredSuras?.filter(sr=>sr.name_arabic.includes(e.target.value)))
-                                }} 
-                                className={'w-full'}
-                            type="text" placeholder="بحث عن سورة"
-                        />
-                 </p>
-         
             </div>
           
-            <div className='flex items-center justify-between w-full flex-wrap'>
+            <div className='
+                    lg:bg-gray-100 lg:p-5
+                    lg:rounded-2xl
+                    flex 
+                    lg:w-4/5 items-center justify-between max-sm:w-full flex-wrap
+                    lg:max-h-[80vh] lg:overflow-y-scroll 
+                    lg:[&::-webkit-scrollbar]:w-2
+                     lg:[&::-webkit-scrollbar-track]:bg-gray-100
+                    lg:[&::-webkit-scrollbar-thumb]:bg-gray-300
+                     lg:dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+                     lg:dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500
+                    '
+                    >
                 
                 {
                      surahSearch === null ? filteredSuras?.map((sr)=>
                         <div
                             key={sr.id}
                             onClick={()=>setSurahPlayer(sr)}
-                            className={`m-2 border text-center shadow p-4 rounded max-sm:w-28 w-32
+                            className={`m-2 border text-center shadow p-4 rounded-xl max-sm:w-28 w-32
                                        hover:cursor-pointer hover:bg-yellow-500
                                        hover:scale-x-105 hover:text-white  
-                                       ${surahPlayer?.id == sr.id && 'bg-yellow-500 text-white'}  
+                                       bg-white
+                                       ${surahPlayer?.id == sr.id && '!bg-yellow-500 text-white'}  
                             `}
                         >
                            <p className='underline khatArabie'>
@@ -149,10 +164,11 @@ export default function Kuran() {
                             <div
                                 key={sr.id}
                                 onClick={()=>setSurahPlayer(sr)}
-                                className={`m-2 border text-center shadow p-4 rounded max-sm:w-28 w-32
-                                           hover:cursor-pointer hover:bg-yellow-500
-                                           hover:scale-x-105 hover:text-white  
-                                           ${surahPlayer?.id == sr.id && 'bg-yellow-500 text-white'}  
+                                className={`m-2 border text-center shadow p-4 rounded-xl max-sm:w-28 w-32
+                                       hover:cursor-pointer hover:bg-yellow-500
+                                       hover:scale-x-105 hover:text-white  
+                                       bg-white
+                                       ${surahPlayer?.id == sr.id && '!bg-yellow-500 text-white'} 
                                 `}
                             >
                                <p className='underline khatArabie'>
